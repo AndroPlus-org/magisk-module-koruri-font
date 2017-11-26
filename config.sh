79e2f598d3191217fa855207c142ab9fa86d93b5
AndroPlus-org <hosigumayuugi@gmail.com>
2017-11-26 14:45:19 +0900

    Fixed broken colon in lockscreen clock
    * You have to edit regular font to add "fancy colon" \uee01

--- config.sh
+++ config.sh
@@ -66,6 +66,24 @@ REPLACE="
 # Construct your own list here, it will overwrite the example
 # !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
+/system/fonts/Roboto-Black.ttf
+/system/fonts/Roboto-BlackItalic.ttf
+/system/fonts/Roboto-Bold.ttf
+/system/fonts/Roboto-BoldItalic.ttf
+/system/fonts/RobotoCondensed-Bold.ttf
+/system/fonts/RobotoCondensed-BoldItalic.ttf
+/system/fonts/RobotoCondensed-Italic.ttf
+/system/fonts/RobotoCondensed-Light.ttf
+/system/fonts/RobotoCondensed-LightItalic.ttf
+/system/fonts/RobotoCondensed-Regular.ttf
+/system/fonts/Roboto-Italic.ttf
+/system/fonts/Roboto-Light.ttf
+/system/fonts/Roboto-LightItalic.ttf
+/system/fonts/Roboto-Medium.ttf
+/system/fonts/Roboto-MediumItalic.ttf
+/system/fonts/Roboto-Regular.ttf
+/system/fonts/Roboto-Thin.ttf
+/system/fonts/Roboto-ThinItalic.ttf
 "
 
 ##########################################################################################
