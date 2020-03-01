2154b00497c49d5c60158eb6f9bd231af472e122
AndroPlus-org <hosigumayuugi@gmail.com>
2020-03-01 11:08:12 +0900

    Fix for OnePlus

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -13,3 +13,12 @@ cp /dev/magisk/mirror/system/etc/fonts.xml $MODDIR/system/etc
 
 #Change fonts.xml file
 sed -i 's@<font weight="400" style="normal" index="0">NotoSansCJK-Regular.ttc</font>@<font weight="300" style="normal">Koruri-Light.ttf</font>\n        <font weight="400" style="normal">Koruri-Regular.ttf</font>\n        <font weight="600" style="normal">Koruri-Semibold.ttf</font>\n        <font weight="700" style="normal">Koruri-Bold.ttf</font>\n        <font weight="800" style="normal">Koruri-Extrabold.ttf</font>@g' $MODDIR/system/etc/fonts.xml
+
+#Copy fonts_slate.xml for OnePlus
+opslate=/dev/magisk/mirror/system/etc/fonts_slate.xml
+if [ -e $opslate ]; then
+    cp $opslate $MODDIR/system/etc
+	
+	#Change fonts_slate.xml file
+	sed -i 's@<font weight="400" style="normal" index="0">NotoSansCJK-Regular.ttc</font>@<font weight="300" style="normal">Koruri-Light.ttf</font>\n        <font weight="400" style="normal">Koruri-Regular.ttf</font>\n        <font weight="600" style="normal">Koruri-Semibold.ttf</font>\n        <font weight="700" style="normal">Koruri-Bold.ttf</font>\n        <font weight="800" style="normal">Koruri-Extrabold.ttf</font>@g' $MODDIR/system/etc/fonts_slate.xml
+fi
